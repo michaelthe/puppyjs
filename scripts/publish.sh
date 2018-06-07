a08773f08875f1276b292415c5803fe7db704e58
michael theodorides <michael.theodorides@circles.bz>
2018-06-07 12:19:26 +0300

    publish patch

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,16 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd ${dir}/..
 
 npm version patch # minor, major
+sleep 1
 
 git add --all
+sleep 1
+
 git commit -vam 'publish patch'
+sleep 1
+
 git push
+sleep 1
 
 npm publish
 
