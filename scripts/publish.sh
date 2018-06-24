5656438bbbeec69029b610e937fa583237f07c15
Unknown <mc.theodorides@gmail.com>
2018-06-24 14:49:31 +0300

    script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,6 +2,8 @@
 
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd $dir/..
+
 NPM_VERSION=$( curl -sL https://unpkg.com/puppyjs/package.json | grep \"version\" | awk -F\" '{ print $4 }' )
 LOCAL_VERSION=$( cat $dir/../package.json | grep \"version\" | awk -F\" '{ print $4 }' )
 
@@ -9,6 +11,9 @@ echo "npm version $NPM_VERSION"
 echo "local version $LOCAL_VERSION"
 
 if [ $NPM_VERSION != $LOCAL_VERSION ] ; then
+    echo "add token"
+    echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/repo/.npmrc
+
     echo "Publishing..."
     npm publish
     echo "Published"
