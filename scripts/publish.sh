bf055bb51461d2853c8288e98c1f73c5d1c014ab
Unknown <mc.theodorides@gmail.com>
2018-06-24 14:44:57 +0300

    publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 NPM_VERSION=$( curl -sL https://unpkg.com/puppyjs/package.json | grep \"version\" | awk -F\" '{ print $4 }' )
 LOCAL_VERSION=$( cat $dir/../package.json | grep \"version\" | awk -F\" '{ print $4 }' )
 
-if [ $NPM_VERSION -ne $LOCAL_VERSION ] ; then
+echo "npm version $NPM_VERSION"
+echo "local version $LOCAL_VERSION"
+
+if [ $NPM_VERSION != $LOCAL_VERSION ] ; then
+    echo "Publishing..."
     npm publish
+    echo "Published"
 fi
