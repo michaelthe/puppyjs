bd5c338b93013df410b4af54dba5abd5bd792b57
Constantinos <constantinos.paschali@circles.bz>
2018-06-26 09:02:25 +0300

    fix tests

--- test/test.e2e.js
+++ test/test.e2e.js
@@ -1,43 +1,42 @@
 describe('test', () => {
-  it('should work with empty url', async () => {
-    const page = await puppy.newPage()
-    await page.waitFor('.test')
+  let page
+
+  afterEach(async () => {
     await page.close()
+  })
+
+  it('should work with empty url', async () => {
+    page = await puppy.newPage()
+    await page.waitFor('.test')
   })
 
   it('should work with index.html', async () => {
-    const page = await puppy.newPage('index.html')
+    page = await puppy.newPage('index.html')
     await page.waitFor('.test')
-    await page.close()
   })
 
   it('should work with /index.html', async () => {
-    const page = await puppy.newPage('/index.html')
+    page = await puppy.newPage('/index.html')
     await page.waitFor('.test')
-    await page.close()
   })
 
   it('should work with localhost:8080', async () => {
-    const page = await puppy.newPage('http://localhost:8080')
+    page = await puppy.newPage('http://localhost:8080')
     await page.waitFor('.test')
-    await page.close()
   })
 
   it('should work with localhost:8080/index.html', async () => {
-    const page = await puppy.newPage('http://localhost:8080/index.html')
+    page = await puppy.newPage('http://localhost:8080/index.html')
     await page.waitFor('.test')
-    await page.close()
   })
 
   it('should work with 127.0.0.1:8080', async () => {
-    const page = await puppy.newPage('http://127.0.0.1:8080')
+    page = await puppy.newPage('http://127.0.0.1:8080')
     await page.waitFor('.test')
-    await page.close()
   })
 
   it('should work with 127.0.0.1:8080/index.html', async () => {
-    const page = await puppy.newPage('http://127.0.0.1:8080/index.html')
+    page = await puppy.newPage('http://127.0.0.1:8080/index.html')
     await page.waitFor('.test')
-    await page.close()
   })
 })
