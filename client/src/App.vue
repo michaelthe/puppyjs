32116b22cd4c3b06dbd798bd477f3e4a15577722
Petros Kyriakou <petros.kyriakou@circles.bz>
2018-06-26 16:09:21 +0300

    setup vuejs, vuetify and parcel

---
+++ client/src/App.vue
@@ -0,0 +1,119 @@
+<template>
+    <v-app id="inspire">
+        <v-navigation-drawer
+                :clipped="$vuetify.breakpoint.lgAndUp"
+                v-model="drawer"
+                fixed
+                app
+        >
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-layout
+                            v-if="item.heading"
+                            :key="item.heading"
+                            row
+                            align-center
+                    >
+                        <v-flex xs6>
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-flex>
+                        <v-flex xs6 class="text-xs-center">
+                            <a href="#!" class="body-2 black--text">EDIT</a>
+                        </v-flex>
+                    </v-layout>
+                    <v-list-group
+                            v-else-if="item.children"
+                            v-model="item.model"
+                            :key="item.text"
+                            :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                            append-icon=""
+                    >
+                        <v-list-tile slot="activator">
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ item.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile
+                                v-for="(child, i) in item.children"
+                                :key="i"
+                                @click=""
+                        >
+                            <v-list-tile-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ child.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list-group>
+                    <v-list-tile v-else :key="item.text" @click="">
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>
+                                {{ item.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar
+                :clipped-left="$vuetify.breakpoint.lgAndUp"
+                color="blue darken-3"
+                dark
+                app
+                fixed
+        >
+            <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                <span class="hidden-sm-and-down">PuppyJS</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            <v-btn icon>
+                <v-icon>apps</v-icon>
+            </v-btn>
+            <v-btn icon large>
+                <v-avatar size="32px" tile>
+                    <img
+                            src="https://vuetifyjs.com/static/doc-images/logo.svg"
+                            alt="Vuetify"
+                    >
+                </v-avatar>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <v-container fluid fill-height>
+                <v-layout justify-center align-center>
+                    <p>I am the main content</p>
+                </v-layout>
+            </v-container>
+        </v-content>
+        </v-btn>
+    </v-app>
+</template>
+
+<script>
+  export default {
+    name: 'app',
+    data: () => ({
+      dialog: false,
+      drawer: null,
+      items: [
+        { icon: 'contacts', text: 'API' },
+        { icon: 'history', text: 'Socket' }
+      ]
+    }),
+    props: {
+      source: String
+    }
+  }
+</script>
---
+++ client/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+
+Vue.use(Vuetify)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
